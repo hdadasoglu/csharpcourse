ae42421a6cf52da928d39c5fcef99259ac05e018
HuseyinDadasoglu <hdadasoglu19@gmail.com>
2022-04-03 16:34:57 +0300

    First Commit

---
+++ Switch_Case/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace if_else
+{
+    public class Program
+    {
+        static void Main(string[] args){
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayıs");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış Giriş");
+                break;
+            }
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkBahar");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış Giriş");
+                    break;
+            }
+        }
+        
+    }
+}
