41b433ddfdadd436467d61161e80b6b602d5309b
HuseyinDadasoglu <hdadasoglu19@gmail.com>
2022-04-03 15:54:23 +0300

    First Commit

---
+++ IF_ELSE/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace if_else
+{
+    public class Program
+    {
+        static void Main(string[] args){
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydın");
+            else if (time > 11)
+                Console.WriteLine("İyi günler");
+            else
+                Console.WriteLine("İyi geceler");
+        
+            Console.WriteLine(time>=6 && time<11? "Günaydın" : time>11 ? "İyi günler":"İyi geceler");
+        }
+        
+    }
+}
