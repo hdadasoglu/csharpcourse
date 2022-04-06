32ce185f09c0ac081d5ae0096aa9034426441ec5
HuseyinDadasoglu <hdadasoglu19@gmail.com>
2022-04-06 21:31:16 +0300

    First Commit

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MethodOverloading{
+    class Program{
+        public static void Main(string[] args){
+            // out parametreler
+            string sayi = "999";
+            bool sonuc = int.TryParse(sayi, out int outSayi); 
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+
+
+            // out parametreler
+            Methodlar methodlar = new Methodlar();
+            methodlar.Topla(2,3,out int toplamsonuc);
+            Console.WriteLine(toplamsonuc);
+
+            // method overloading
+            
+            int ifade = 9999;
+            methodlar.EkranaYazdir(Convert.ToString(ifade));
+            methodlar.EkranaYazdir(ifade);
+            methodlar.EkranaYazdir("Ayşe"+" Ahmet");
+
+
+        }
+    }
+}
+
+class Methodlar{
+    public void Topla(int a, int b, out int toplam){
+        toplam = a+b;
+    }
+    public void EkranaYazdir(string str){
+        Console.WriteLine(str);
+    }
+    public void EkranaYazdir(int str){
+        Console.WriteLine(str);
+    }
+    public void EkranaYazdir(string str, string str2){
+        Console.WriteLine(str + str2);
+    }
+    
+}
