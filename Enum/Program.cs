766ca95b8178c97c61597f882767dff18d26a4e5
HuseyinDadasoglu <hdadasoglu19@gmail.com>
2022-04-11 21:54:56 +0300

    First Commit

---
+++ Enum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enum{
+    class Program{
+        public static void Main(string[] args){
+            int sıcaklık = 17;
+
+            if(sıcaklık<=(int)HavaDurumu.Soguk)
+                Console.WriteLine("Hava Soğuk");
+            else if(sıcaklık<=(int)HavaDurumu.Normal)
+                Console.WriteLine("Hava Normal");
+            else if(sıcaklık<=(int)HavaDurumu.Sıcak)
+                Console.WriteLine("Hava Sıcak");
+            else if(sıcaklık<=(int)HavaDurumu.ÇokSıcak)
+                Console.WriteLine("Hava Çook Sıcak");
+            else
+                Console.WriteLine("Hata");
+        }
+    }
+    enum HavaDurumu{
+        Soguk = 15,
+        Normal = 25,
+        Sıcak = 30,
+        ÇokSıcak = 40
+    }
+}
